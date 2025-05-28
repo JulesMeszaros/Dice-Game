554e9aa26a423e909fd605e1af79ee80524823a5
Jules "n8scape" Meszaros <julesmeszaros@gmail.com>
2025-05-28 22:00:33 +0200

    updated main menu version

--- src/screens/MainMenu.lua
+++ src/screens/MainMenu.lua
@@ -74,7 +74,7 @@ function MainMenu:updateCanvas(dt)
     love.graphics.draw(textTitle, self.mainMenuCanvas:getWidth()/2, 100, 0, 1, 1, textTitle:getWidth()/2, textTitle:getHeight()/2)
 
     --Version
-    local versionText = love.graphics.newText(Fonts.pixelated, "AEROSOL DELUXE GAMES — v0.0.2dev")
+    local versionText = love.graphics.newText(Fonts.pixelated, "AEROSOL DELUXE GAMES — v0.0.3dev")
     love.graphics.draw(versionText, 20, self.mainMenuCanvas:getHeight()-20, 0, 1, 1, 0, versionText:getHeight())
 
     --Buttons
