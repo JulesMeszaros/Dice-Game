2a0c6909186dbd7774343f3e03e613c8b197dc88
Jules "n8scape" Meszaros <julesmeszaros@gmail.com>
2025-08-01 18:29:43 +0200

    disabled shop every round

--- src/utils/constants.lua
+++ src/utils/constants.lua
@@ -18,7 +18,7 @@ local Constants = {
     BASE_TRIGGER_ANIMATION_TIME = 0.15,--secondes
     
     --Debug
-    SHOP_EVERY_DESK = true,
+    SHOP_EVERY_DESK = false,
     
     --Prices
     BASE_COFFEE_PRICE = 5,
