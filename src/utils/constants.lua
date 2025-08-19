4fbeff8f12556f993e7e0598daddb71330d661c6
Jules Meszaros <jules.meszaros@edf.fr>
2025-08-19 11:01:07 +0200

    changed boss labels to avoid bugs

--- src/utils/constants.lua
+++ src/utils/constants.lua
@@ -169,8 +169,8 @@ Constants.BOSS_TYPES = {
 Constants.BOSS_TYPES_DESC = {
     {"Project Manager", "Cannot play numbered figures (ones, twos, threes...)"},
     {"Chief Accountant", "Cannot play non-numbered figures"},
-    {"Trésorier", "Playing a dice costs 1$"},
-    {"R&D Manager", "Rerolls are added to the numbers of turns, and is set to 0"},
+    {"Tresorier", "Playing a dice costs 1$"},
+    {"R/D Manager", "Rerolls are added to the numbers of turns, and is set to 0"},
     {"Safety Advisor", "Cannot use magic wands this round"}
 }
 
