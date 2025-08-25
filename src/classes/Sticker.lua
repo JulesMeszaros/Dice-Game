097f0784fd253e9b1a7328f825bf94890aebdaed
Jules "n8scape" Meszaros <julesmeszaros@gmail.com>
2025-08-25 11:06:38 +0200

    added a comment todo

--- src/classes/Sticker.lua
+++ src/classes/Sticker.lua
@@ -4,6 +4,10 @@ Sticker.__index = Sticker
 function Sticker:new()
 	local self = setmetatable({}, Sticker)
 
+	self.name = "Sticker"
+	self.description = "This is a placeholder Sticker. You Are not supposed to see this.... woops"
+	self.holographic = false
+
 	return self
 end
 
