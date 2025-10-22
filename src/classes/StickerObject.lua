631087d700046786460b9649bef24058d579fa74
Jules "n8scape" Meszaros <julesmeszaros@gmail.com>
2025-10-22 12:07:27 +0200

    ajouté quelques fonctions dans la classe sticker object, implémenté un premier type de sticker

--- src/classes/StickerObject.lua
+++ src/classes/StickerObject.lua
@@ -12,29 +12,53 @@ function Sticker:new()
 	return self
 end
 
-function Sticker:startRoundEffect() end
+function Sticker:startRoundEffect()
+	print("StartRound")
+end
 
-function Sticker:endRoundEffect() end
+function Sticker:endRoundEffect()
+	print("endRound")
+end
 
-function Sticker:figurePlayedEffect() end
+function Sticker:figurePlayedEffect()
+	print("figurePlyed")
+end
 
-function Sticker:startTriggerEffect() end
+function Sticker:startTriggerEffect()
+	print("starttrigger")
+end
 
-function Sticker:endTriggerEffect() end
+function Sticker:endTriggerEffect()
+	print("endtrigger")
+end
 
-function Sticker:diceTriggeredEffect() end
+function Sticker:diceTriggeredEffect()
+	print("diceTrigger")
+end
 
-function Sticker:rerollEffect() end
+function Sticker:rerollEffect()
+	print("reroll")
+end
 
-function Sticker:caseEffect() end
+function Sticker:caseEffect()
+	print("caseeffect")
+end
 
-function Sticker:ciggieUsedEffect() end
+function Sticker:ciggieUsedEffect()
+	print("ciggieUsed")
+end
 
-function Sticker:coffeeUsedEffect() end
+function Sticker:coffeeUsedEffect()
+	print("coffeUsed")
+end
 
-function Sticker:diceFaceBoughtEffect() end
+function Sticker:diceFaceBoughtEffect()
+	print("diceFaceBought")
+end
 
---Effet qui se déclenche une seule fois, au moment ou le sticker est acheté et placé sur le badge
-function Sticker:buyEffect() end
+--Effet qui se déclenche une seule fois, au moment ou le sticker est acheté et placé sur le terrain
+function Sticker:buyEffect()
+	print("sticker bought")
+end
 
 return Sticker
---
+++ src/classes/StickerTypes.lua
@@ -0,0 +1,14 @@
+local StickerObject = require("src.classes.StickerObject")
+
+local StickerTypes = {}
+
+local FlameSticker = setmetatable({}, { __index = StickerObject })
+FlameSticker.__index = FlameSticker
+
+function FlameSticker:new()
+	local self = setmetatable(StickerObject:new(), FlameSticker)
+
+	return self
+end
+
+return StickerTypes
